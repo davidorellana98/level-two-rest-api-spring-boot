29dd86d3865985ac9cda1f5b6326b692cb3431f3
David Orellana <davidorellana_98@hotmail.com>
2022-11-12 14:50:11 -0500

    Initial commit

---
+++ src/main/java/com/davidorellana/leveltworestapi/LevelTwoRestApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.davidorellana.leveltworestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelTwoRestApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelTwoRestApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidorellana/leveltworestapi/LevelTwoRestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidorellana.leveltworestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelTwoRestApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
